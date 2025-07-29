38e4be59baf25fbea315f8bc5586216ba89ca0f2
lime360 <misnikovk1@gmail.com>
2025-07-29 18:19:40 +0300

    installer fix

--- install.sh
+++ install.sh
@@ -3,4 +3,12 @@
 npm install
 npm run make
 mv out/cord-linux-x64 $HOME/.local/cord
-cp Cord.desktop $HOME/.local/share/applications/Cord.desktop
+echo "[Desktop Entry]
+Type=Application
+Name=Cord
+Exec=$HOME/.local/cord/cord
+Comment=Discord web client wrapper with disabled telemetry and custom theme support
+Terminal=false
+Icon=discord
+Categories=GNOME;GTK;Network;
+PrefersNonDefaultGPU=false" > $HOME/.local/share/applications/Cord.desktop
